7a756d15b55f19b56688b2549b3e843b007d671b
Universalizer <116723165+Universalizer@users.noreply.github.com>
2023-02-08 16:32:38 +0530

    Update updateallbranchestags.sh

--- updateallbranchestags.sh
+++ updateallbranchestags.sh
@@ -1,4 +1,4 @@
 remote=origin ; for brname in `git branch -r | grep origin | grep -v master | grep -v HEAD | sed -e 's/^[^\/]*\///'`; do git branch --track $brname $remote/$brname ; done 
 $ git pull --rebase --all
-git push --all Stealth
-git push --tags Stealth
+$ git push --all Stealth
+$ git push --tags Stealth
