d801761af9c46afa51792408ec60e33a1b283bd3
Universalizer <116723165+Universalizer@users.noreply.github.com>
2023-02-08 16:26:30 +0530

    Update updateallbranchestags.sh

--- updateallbranchestags.sh
+++ updateallbranchestags.sh
@@ -1,2 +1,2 @@
 remote=origin ; for brname in `git branch -r | grep origin | grep -v master | grep -v HEAD | sed -e 's/^[^\/]*\///'`; do git branch --track $brname $remote/$brname ; done 
-git pull --rebase --all
+$ git pull --rebase --all
